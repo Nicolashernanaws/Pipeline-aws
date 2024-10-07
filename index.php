cc965a0994e89766b8076f3bd383304b2f02671f
Nicolasaws <151962674+Nicolashernanaws@users.noreply.github.com>
2024-10-07 20:20:47 -0300

    Update index.php
    aaa

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 <body>
     <div class="content">
         <?php
-        echo "Hello World! NICO<br>";
+        echo "Hello World!aaa<br>";
         echo "Server: " . gethostname() . "<br>";
         echo "Web server #" . getenv('SERVER_ID');
         ?>
